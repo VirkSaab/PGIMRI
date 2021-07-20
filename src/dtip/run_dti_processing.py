34544620de4b0c9c1d805c1c49ae3ae02881c0a2
jsvirk <krivsj@gmail.com>
2021-07-20 22:25:29 +0530

    adding pgi_pc code to legion then main branch

--- src/dtip/run_dti_processing.py
+++ src/dtip/run_dti_processing.py
@@ -1,3 +1,25 @@
 import os
-import 
+import click
 
+
+
+
+def run_one_subject(datadir):
+    datadir = os.path.abspath(datadir)
+    print(datadir)
+
+
+@click.command()
+@click.argument("datadir")
+# @click.option("-d", "--datadir", type=str, required=True, help="path to subject data folder")
+def dtip(datadir:str) -> None:
+    """Run DTI processing for ITC cases.
+    DATADIR - path to subject's data folder.
+    """
+    ret_out = run_one_subject(datadir)
+    click.echo("DONE!")
+    pass
+
+
+if __name__ == "__main__":
+    dtip()
