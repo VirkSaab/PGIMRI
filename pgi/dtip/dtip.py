fc5daa43c406eb8d701e644908224c41a96750d1
jsvirk <krivsj@gmail.com>
2021-07-22 00:21:35 +0530

    Added two spincursor classes and DTI dicom2nifti step

--- pgi/dtip/dtip.py
+++ pgi/dtip/dtip.py
@@ -2,7 +2,7 @@ import dicom2nifti
 import zipfile
 from typing import Union
 from pathlib import Path
-from pgi.utils import get_logger
+from pgi.utils import get_logger, SpinCursor2
 
 
 logger = get_logger(__name__, to_file=f"dti_processing.log")
@@ -20,21 +20,34 @@ def process_one_subject(subject_path: Union[str, Path],
         > compression -- compress .nii file to .nii.gz.
         > reorient -- reorient the dicoms according to LAS orientation.
     """
-
+    subject_path = Path(subject_path)
     output_folder = Path(output_folder)
-    logger.debug(f"outputs will be saved at {output_folder}")
+
+    logger.debug(f"outputs will be saved at `{output_folder}`")
+    
     # Step 1: Convert DICOM images to NIfTI format
-    extract_to_path = output_folder/"1_nifti"
-    extract_to_path.mkdir(parents=True, exist_ok=True)
+    nifti_path = output_folder/"1_nifti"
+    nifti_path.mkdir(parents=True, exist_ok=True)
 
     # unzip if given input is a zip file
-    # if os.path.isfile(subject_path):
+    if subject_path.is_file():
+        extract_to_path = output_folder/"0_extracted_files"
+        extract_to_path.mkdir(parents=True, exist_ok=True)
 
-    #     with zipfile.ZipFile(subject_path, 'r') as zip_ref:
-    #         zip_ref.extractall(extract_to_path)
-    #     subject_path = extract_to_path
+        logger.info(f"Extracting files to `{extract_to_path}`...")
+        
+        with zipfile.ZipFile(subject_path, 'r') as zip_ref:
+            zip_ref.extractall(extract_to_path)
 
-    # dicom2nifti.convert_directory(
-    #     dicom_directory=subject_path, output_folder=output_folder,
-    #     compression=compression, reorient=reorient
-    # )
+        logger.info(f"done.")
+
+        subject_path = extract_to_path
+
+    logger.info(f"Converting DICOM to NIfTI...")
+    with SpinCursor2("dicom2nifti..."):
+        dicom2nifti.convert_directory(
+            dicom_directory=subject_path, output_folder=nifti_path,
+            compression=compression, reorient=reorient
+        )
+    
+    logger.info(f"done.")
--- pgi/utils.py
+++ pgi/utils.py
@@ -1,11 +1,19 @@
 import logging
 import logging.config
 import click_logging
+import sys
+import time
+from itertools import cycle
+from shutil import get_terminal_size
+from threading import Thread
 
+__all__ = [
+    "get_logger",
+    "SpinCursor", "SpinCursor2"
 
-__all__ = ["get_logger"]
+]
 
-
+# ================================== FUNCTIONS ###############################
 def get_logger(name: str, log_level="DEBUG",
                to_file: str = '', file_log_level="DEBUG") -> logging.Logger:
     echo_kwargs = {
@@ -15,23 +23,121 @@ def get_logger(name: str, log_level="DEBUG",
     }
     style_kwargs = {
         'error': dict(fg='red', blink=True),
-        'exception': dict(fg='red', blink=True),
         'critical': dict(fg='red', blink=True),
-        'info': dict(fg='cyan', blink=True)
+        'warning': dict(fg='orange', blink=True),
+        'exception': dict(fg='yellow', blink=True),        
+        'info': dict(fg='cyan', blink=True),
+        'debug': dict(fg='green', blink=True)
     }
     logger = logging.getLogger(f"logger_name: {name}")
+    logger.setLevel(log_level)
     ch = click_logging.ClickHandler(echo_kwargs=echo_kwargs)
-    ch.setLevel(eval(f"logging.{log_level}"))
+    ch.setLevel(log_level)
     ch.setFormatter(click_logging.ColorFormatter(style_kwargs=style_kwargs))
     logger.addHandler(ch)
 
     if to_file != '':
         fh = logging.FileHandler(to_file)
-        fh.setLevel(eval(f"logging.{file_log_level}"))
+        fh.setLevel(file_log_level)
         formatter = logging.Formatter(
-            '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            '%(asctime)s - %(name)s - [%(levelname)s] - %(message)s',
             datefmt='%d/%B/%Y %I:%M:%S %p')
         fh.setFormatter(formatter)
         logger.addHandler(fh)
 
     return logger
+
+
+# ============================== CLASSES ==============================
+class SpinCursor(Thread):
+    """ A console spin cursor class 
+    """
+    
+    def __init__(self, cursor_type:str="bar"):
+        """Choose one of `spin` and `bar`"""
+        self.t = Thread(target=self.animate)
+        # This will make the thread stop if the main process
+        # is killed (i.e. from a KeyboardInterrupt)
+        self.t.daemon=True
+
+        if cursor_type == 'bar':
+            self.bar = [
+                " [=     ]",
+                " [ =    ]",
+                " [  =   ]",
+                " [   =  ]",
+                " [    = ]",
+                " [     =]",
+                " [    = ]",
+                " [   =  ]",
+                " [  =   ]",
+                " [ =    ]",
+            ]
+        elif cursor_type == 'spin':
+            self.bar = ['|', '/', '-', '\\']
+        else:
+            raise NotImplementedError("choose one [`spin`, `bar`].")
+
+        self.bar_len = len(self.bar)
+        self.flag = True
+        
+    def animate(self):
+        i = 0
+        while self.flag:
+            # print(self.bar[i % self.bar_len], end="\r")
+            sys.stdout.write('\r' + self.bar[i % self.bar_len])
+            sys.stdout.flush()
+            time.sleep(.15)
+            i += 1
+        sys.stdout.write('              \ndone!             ')
+        sys.stdout.write('\n')
+
+    def start(self):
+        self.t.start()
+
+    def stop(self):
+        self.flag = False
+
+
+class SpinCursor2:
+    def __init__(self, desc="Loading...", end="Done!", timeout=0.1):
+        """
+        A loader-like context manager
+        source: https://stackoverflow.com/questions/22029562/python-how-to-make-simple-animated-loading-while-process-is-running
+
+        Args:
+            desc (str, optional): The loader's description. Defaults to "Loading...".
+            end (str, optional): Final print. Defaults to "Done!".
+            timeout (float, optional): Sleep time between prints. Defaults to 0.1.
+        """
+        self.desc = desc
+        self.end = end
+        self.timeout = timeout
+
+        self._thread = Thread(target=self._animate, daemon=True)
+        self.steps = ["⢿", "⣻", "⣽", "⣾", "⣷", "⣯", "⣟", "⡿"]
+        self.done = False
+
+    def start(self):
+        self._thread.start()
+        return self
+
+    def _animate(self):
+        for c in cycle(self.steps):
+            if self.done:
+                break
+            print(f"\r{self.desc} {c}", flush=True, end="")
+            time.sleep(self.timeout)
+
+    def __enter__(self):
+        self.start()
+
+    def stop(self):
+        self.done = True
+        cols = get_terminal_size((80, 20)).columns
+        print("\r" + " " * cols, end="", flush=True)
+        print(f"\r{self.end}", flush=True)
+
+    def __exit__(self, exc_type, exc_value, tb):
+        # handle exceptions with those variables ^
+        self.stop()
